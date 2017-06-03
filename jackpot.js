7c63a58b5911d6f3e563dd55bff33a9ba7d4ab07
Benedict Florance <benedictflorance.a@gmail.com>
2017-06-03 14:10:39 +0530

    Rectified Aligning issues and Modified Picture Mode

--- jackpot.js
+++ jackpot.js
@@ -7,13 +7,14 @@ var count=0;
 var countpic=0;
 var picpoints=0;
 var incr=Math.floor(Math.random()*20+1);
+var incrpic=Math.floor(Math.random()*20+1);
 function addNos(e){
 	count++;
 	e.preventDefault();
 	//To increase the probability for winning, at some random try within 20 all the boxes are made the same number
 	if(incr==count)
 	{b1.textContent=b2.textContent=b3.textContent=Math.floor(Math.random()*9+1);
-		incr+=20;}
+	incr+=20;}
 	else{
 	b1.textContent=Math.floor(Math.random()*9+1);
 	b2.textContent=Math.floor(Math.random()*9+1);
@@ -32,17 +33,17 @@ function addPics(e){
 	countpic++;
 	e.preventDefault();
 	//To increase the probability for winning, at some random try within 20 all the boxes are made the same number
-	if(incr==countpic)
-	{b1.innerHTML=b2.innerHTML=b3.innerHTML="<img src=\"images\\"+Math.floor(Math.random()*9+1)+".png\" id=\"setsize\" />"
-		incr+=20;}
+	if(incrpic==countpic)
+	{	
+	b1.innerHTML=b2.innerHTML=b3.innerHTML="<img src=\"images\\"+Math.floor(Math.random()*9+1)+".png\" id=\"setsize\" />";		
+	incrpic+=20;}
 	else{
 	var a=Math.floor(Math.random()*9+1);
 	var b=Math.floor(Math.random()*9+1);
 	var c=Math.floor(Math.random()*9+1);
-	b1.innerHTML="<img src=\"images\\"+a+".png\" id=\"setsize\" />"
-	b2.innerHTML="<img src=\"images\\"+b+".png\" id=\"setsize\" />"
-	b3.innerHTML="<img src=\"images\\"+c+".png\" id=\"setsize\" />"
-	}
+	b1.innerHTML="<img src=\"images\\"+a+".png\" id=\"setsize\" />";
+	b2.innerHTML="<img src=\"images\\"+b+".png\" id=\"setsize\" />";
+	b3.innerHTML="<img src=\"images\\"+c+".png\" id=\"setsize\" />";
 	if(a==b&b==c)
 	{	picpoints+=50;
 		stat.innerHTML="Congrats, You won! :) <br><br> Points: "+picpoints;
@@ -51,11 +52,17 @@ function addPics(e){
 	{	picpoints--;
 		stat.innerHTML="Better luck, next time :/ <br><br> Points: "+picpoints;
 	}
+	}
+	
+}
+function picMode(e){
+e.preventDefault();
+spinEl.addEventListener('click', addPics, false);
 }
 
 var spinEl = document.getElementById('spin');
 spinEl.addEventListener('click', addNos, false);
 
 var picEl = document.getElementById('pictures');
-picEl.addEventListener('click', addPics, false);
+picEl.addEventListener('click', picMode, false);
 
